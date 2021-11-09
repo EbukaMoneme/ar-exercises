995860b7228ede70dd68e6ebe7908a061ae896ae
EbukaMoneme <camoneme@gmail.com>
2021-11-09 14:35:43 +0000

    Use active record to establish a one to many association between models

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,10 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+#adding data into employees
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Timmy", last_name: "Turner", hourly_rate: 70)
+@store1.employees.create(first_name: "Darth", last_name: "Vader", hourly_rate: 99)
+@store2.employees.create(first_name: "Bob", last_name: "Saget", hourly_rate: 40)
+@store2.employees.create(first_name: "Bob", last_name: "TheBuilder", hourly_rate: 20)
+@store2.employees.create(first_name: "Sheldon", last_name: "Cooper", hourly_rate: 110)
